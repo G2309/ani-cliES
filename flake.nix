b271dbe51007f589b53ccd9c29dbb1a3f8b028e9
Gustavo Cruz <cru22779@uvg.edu.gt>
2025-12-15 19:11:52 -0600

    [CHORE] nix flake migration

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "anicli-es - CLI para ver anime en español";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      
+      python = pkgs.python3.withPackages (ps: [
+        ps.beautifulsoup4
+        ps.requests
+      ]);
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          python
+          pkgs.mpv
+          pkgs.fzf
+        ];
+
+      };
+    };
+}
